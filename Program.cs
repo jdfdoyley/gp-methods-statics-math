a376be9bfe306280e8921ada7da8dfe1f3eed58f
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-18 05:28:03 -0400

    Accept three floating-point values from the user

--- Program.cs
+++ Program.cs
@@ -18,5 +18,24 @@ public class StaticsMethodsMath
         Console.WriteLine(
             "\nJason D'Oyley - Week 3 GP Methods, Statics, Class Math\n"
         );
+
+        // Get 3 floating point values from the user
+        Console.Write(
+            "Enter 3 floating-point values separated by spaces: "
+        );
+        string? val = Console.ReadLine();
+
+        // Check to make sure a value was entered before trying to use it
+        if (!string.IsNullOrEmpty(val))
+        {
+            // Use the string.Split method to break the input string into an
+            // array based on whitespace in the string
+            string[] vals = val.Split();
+            double f1 = double.Parse(vals[0]);
+            double f2 = double.Parse(vals[1]);
+            double f3 = double.Parse(vals[2]);
+
+            Console.WriteLine($"{f1}\n{f2}\n{f3}");
+        }
     }
 }
