84aea74762ad42e996cb823f28542d73c0aa02d0
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-18 05:17:25 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: October 18, 2022
+* Assignment CIS214 Week 3 GP - Methods, Statics, Class Math
+*
+* Main application (program) class.
+* In this application we will demonstrate the creation and use of user-defined
+* methods, static methods, method overloading, and the use of the Math Class.
+\******************************************************************************/
+public class StaticsMethodsMath
+{
+    // Static member variable - belongs to the App class
+    private static string Label = "The maximum value is: ";
+
+    static void Main(string[] args)
+    {
+        // Print a header line
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Methods, Statics and Math Class
+
+In this application I will demonstrate the creation and use of user-defined methods, static methods, method overloading, and the use of the Math class.
